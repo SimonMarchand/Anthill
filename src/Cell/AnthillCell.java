3b6787d1cfaba5e66109a4e5c8f66f4c9c26963a
Julie Mounier <juliemounier96@gmail.com>
2017-01-03 17:53:55 +0100

    AnthillCell's constructor implemented

--- src/Cell/AnthillCell.java
+++ src/Cell/AnthillCell.java
@@ -3,5 +3,19 @@ package Cell;
 /**
  * Created by felix on 03/01/17.
  */
-public class AnthillCell {
+public class AnthillCell extends Cell {
+
+	private int foodQuantity;
+	
+
+	public AnthillCell() {
+		super(0);
+		this.foodQuantity = 0;
+	}
+	
+	
+	public void placeFood(int foodQuantity) {
+		this.foodQuantity += foodQuantity;
+	}	
+	
 }
