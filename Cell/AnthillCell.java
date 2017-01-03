97452f73dad6c31b258fa129f6530804cc8c88d5
Fabien Ogli <pp.btsig@free.fr>
2017-01-03 17:19:03 +0100

    fourmillière

--- Cell/AnthillCell.java
+++ Cell/AnthillCell.java
@@ -4,4 +4,19 @@ package Cell;
  * Created by felix on 03/01/17.
  */
 public class AnthillCell {
+	
+	private int foodQuantity;
+	
+	public AnthillCell(){
+		foodQuantity = 0;
+	}
+	
+	public void setFood(int food){
+		foodQuantity += food;
+	}
+	
+	public int getQuantity(){
+		return foodQuantity;
+	}
+	
 }
