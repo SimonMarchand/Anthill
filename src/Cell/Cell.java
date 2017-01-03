2a94947cb534af1e00d1396ce201bca3dd3b1991
Julie Mounier <juliemounier96@gmail.com>
2017-01-03 17:36:55 +0100

    Cell & emptycell constructors

--- src/Cell/Cell.java
+++ src/Cell/Cell.java
@@ -4,4 +4,12 @@ package Cell;
  * Created by felix on 03/01/17.
  */
 public class Cell {
+
+	private int pheromoneHistory;
+	
+	public Cell(int pheromoneHistory) {
+		super();
+		this.pheromoneHistory = pheromoneHistory;
+	}
+
 }
--- src/Cell/EmptyCell.java
+++ src/Cell/EmptyCell.java
@@ -3,5 +3,10 @@ package Cell;
 /**
  * Created by felix on 03/01/17.
  */
-public class EmptyCell {
+public class EmptyCell extends Cell {
+
+	public EmptyCell() {
+		super(0);
+	}
+	
 }
