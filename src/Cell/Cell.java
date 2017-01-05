99dfce464544432fe011918fe6cffd8c04d895ec
Julie Mounier <juliemounier96@gmail.com>
2017-01-05 11:22:40 +0100

    Update Cell & ObstacleCell

--- src/Cell/Cell.java
+++ src/Cell/Cell.java
@@ -1,5 +1,7 @@
 package Cell;
 
+import MapManagement.Map;
+
 /**
  * Created by felix on 03/01/17.
  */
@@ -7,11 +9,34 @@ public class Cell {
 
 	private int pheromoneHistory;
 	private Coordinates coord;
+	private Pheromone pheromone;
+	private Map map;
 	
-	public Cell(int pheromoneHistory,int x, int y) {
-		super();
-		this.pheromoneHistory = pheromoneHistory;
-		coord = new Coordinates(x,y);
+	
+	public Cell(Coordinates coord, Map map) {
+		this.pheromoneHistory = 0;
+		this.coord = coord;
+		this.pheromone = new Pheromone(0);
+		this.map = map;
+	}
+	
+	
+	public int getPheromoneHistory() { return pheromoneHistory; }
+	public void setPheromoneHistory(int pheromoneHistory) { this.pheromoneHistory = pheromoneHistory; }
+
+	public Coordinates getCoord() { return coord; }
+	public void setCoord(Coordinates coord) { this.coord = coord; }
+
+	public Pheromone getPheromone() { return pheromone; }
+	public void setPheromone(Pheromone pheromone) { this.pheromone = pheromone; }
+
+	public Map getMap() { return map; }
+	public void setMap(Map map) { this.map = map; }
+
+	
+	public void putPheromones(Pheromone nbPheromone){
+		this.pheromoneHistory += 1;
+		this.pheromone = nbPheromone;
 	}
 
 }
