696777bda1903ae40dd2a56f124de317ed524cb1
Fabien Ogli <pp.btsig@free.fr>
2017-01-05 11:57:57 +0100

    getteur et setteur de food

--- src/Cell/FoodCell.java
+++ src/Cell/FoodCell.java
@@ -1,11 +1,24 @@
 package Cell;
 
+import MapManagement.Map;
 /**
  * Created by felix on 03/01/17.
  */
 public class FoodCell extends Cell {
+	
+	private Food food;
+	
 
-	public FoodCell(int pheromoneHistory) {
-		super(pheromoneHistory);
+	public FoodCell(Coordinates coord, Map map, Food food) {
+		super(coord, map);
+		this.food = food;
+	}
+	
+	public Food getFood(){
+		return food;
+	}
+	
+	public void setFood(Food food){
+		this.food = food;
 	}
 }
