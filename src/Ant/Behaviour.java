27d518802f7c12e9958f482f75b505eb3a26c594
Félix Sabatié <felix.sabatie@gmail.com>
2017-01-12 11:33:08 +0100

    Vanilla Behaviour done // TO TEST
    Méthode getChosenCell réutilisable pour le SensorBehaviour

--- src/Ant/Behaviour.java
+++ src/Ant/Behaviour.java
@@ -5,6 +5,7 @@ import Cell.Coordinates;
 import MapManagement.Map;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by felix on 03/01/17.
@@ -13,6 +14,8 @@ public abstract class Behaviour {
     // Contient toutes les cellules entourant la fourmi
     protected ArrayList<CellEvaluation> surroundings;
     protected Ant ant;
+    // Contient la somme pondérée des évaluations des cases dans le sens horaire
+    protected ArrayList<Float> evaluations;
 
     /**
      * Renvoie la cellule du prochain mouvement de la fourmi donnée en paramètre.
@@ -28,6 +31,9 @@ public abstract class Behaviour {
         return ant.getAntHill().getMap().getCell(ant.getBackTrack().pop());
     }
 
+    /**
+     * Set les cellules entourant la fourmi, en commançant par la case devant elle puis dans le sens horaire
+     */
     protected void setSurroundingCells() {
         this.surroundings = new ArrayList<CellEvaluation>();
 
@@ -109,4 +115,51 @@ public abstract class Behaviour {
 
         return cell;
     }
+
+    /**
+     * Retourne la cellule choisie aléatoirement par la fourmi à partir
+     * du tableau d'évaluation rempli par les classes filles.
+     *
+     * @return cell: Cell
+     */
+    protected Cell getChosenCell() {
+        Random r = new Random();
+        float randomEvaluation = r.nextFloat();
+        Cell cell = null;
+
+        /**
+         * On veut éviter de donner une case ayant une évaluation de 0.
+         * Une évaluation de 0 correspond dans le tableau d'évaluation à 2 valeurs identiques consécutives.
+         * En testant sur une égalité non stricte, on est sûrs de choisir en premier une case possédant une évaluation
+         * non nulle, sauf dans le cas où le float tiré est égal à 0. Dans ce cas, on dit que la fourmi ne bouge pas pour
+         * éviter des erreurs.
+          */
+        if(randomEvaluation == 0) {
+            cell = ant.getCurrentCell();
+        }
+        else if(randomEvaluation <= this.evaluations.get(0)) {
+            ant.setOrientation(Ant.ORIENTATIONS[ (Ant.getOrientationIndex(ant.getOrientation()) + 0) % 8 ]);
+            cell = this.surroundings.get(0).getCell();
+        }
+        else {
+            int i = 1;
+            while (cell == null || i != this.evaluations.size()) {
+                if(randomEvaluation >= this.evaluations.get(i - 1) && randomEvaluation <= this.evaluations.get(i)) {
+                    ant.setOrientation(Ant.ORIENTATIONS[ (Ant.getOrientationIndex(ant.getOrientation()) + i) % 8 ]);
+                    cell = this.surroundings.get(i).getCell();
+                }
+                i++;
+            }
+        }
+
+        // Si jamais la cellule est nulle, on fait en sorte que la fourmi ne bouge pas.
+        if(cell == null)
+            cell = ant.getCurrentCell();
+
+        if(cell.getClass().getName() == "ObstacleCell")
+                cell = ant.getCurrentCell();
+
+        return cell;
+    }
+
 }
--- src/Ant/VanillaBehaviour.java
+++ src/Ant/VanillaBehaviour.java
@@ -2,15 +2,21 @@ package Ant;
 
 import Cell.Cell;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by felix on 03/01/17.
+ * Implémentation du comportement de la fourmi sans prise en compte des phéromones,
+ * et la fourmi ne voit pas les cases autour d'elle. Elle peut choisir de se déplacer vers
+ * une case obstacle, auquel cas elle s'y dirige mais reste sur place.
  */
 public class VanillaBehaviour extends Behaviour {
-    private static int EVAL_MAX = 20;
-    private static int EVAL_2 = 15;
-    private static int EVAL_3 = 10;
-    private static int EVAL_4 = 5;
-    private static int EVAL_MIN = 0;
+    private static float EVAL_MAX = 20;
+    private static float EVAL_2 = 15;
+    private static float EVAL_3 = 10;
+    private static float EVAL_4 = 5;
+    private static float EVAL_MIN = 0;
 
     public VanillaBehaviour(Ant ant) {
         this.ant = ant;
@@ -22,17 +28,17 @@ public class VanillaBehaviour extends Behaviour {
 
         this.setSurroundingCells();
         this.setSurroundingEvaluations();
-        // Trier les surroundings par évaluation puis retourner la cell la plus appropriée
 
-        return null;
+        return getChosenCell();
     }
 
     @Override
     protected void setSurroundingEvaluations() {
+        float evaluationsSum = 0;
+        this.evaluations = new ArrayList<Float>();
+
         for (int i = 0; i < this.surroundings.size(); i++) {
-            if (this.surroundings.get(i).getCell().getClass().getName() == "ObstacleCell") {
-                this.surroundings.get(i).setEvaluation(-1);
-            } else if (i == 0) {
+            if (i == 0) {
                 this.surroundings.get(i).setEvaluation(EVAL_MAX);
             } else if (i == 1 || i == this.surroundings.size() - 1) {
                 this.surroundings.get(i).setEvaluation(EVAL_2);
@@ -43,6 +49,17 @@ public class VanillaBehaviour extends Behaviour {
             } else {
                 this.surroundings.get(i).setEvaluation(EVAL_MIN);
             }
+            evaluationsSum += this.surroundings.get(i).getEvaluation();
         }
+
+        for (CellEvaluation cell : this.surroundings) {
+            cell.setEvaluation(cell.getEvaluation() / evaluationsSum);
+            // Si on a déjà des évaluations dans le tableau, on ajoute l'évaluation précédente du tableau
+            if(this.evaluations.size() != 0)
+                this.evaluations.add(cell.getEvaluation() + this.evaluations.get(this.evaluations.size() - 1));
+            else
+                this.evaluations.add(cell.getEvaluation());
+        }
+
     }
 }
